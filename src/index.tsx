eca04e0e5631887a20f6965cb32b555652bd22a5
Derrick Hawkins <dhawkvq@yahoo.com>
2021-08-21 22:38:55 -0700

    BookProvider wrapped and delivering context to App

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BookProvider } from './context/BookContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BookProvider>
+     <App />
+    </BookProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
